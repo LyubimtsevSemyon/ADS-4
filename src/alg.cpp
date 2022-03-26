679cf3b357a70e83ed902f4b951beca31d1cf0e1
LyubimtsevSemyon <99214966+LyubimtsevSemyon@users.noreply.github.com>
2022-03-26 19:32:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,60 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+int count = 0;
+for (int i = 0; i < len; i++) {
+for (int j = i + 1; j < len; j++) {
+if (arr[i] + arr[j] == value)
+  count ++;
+}
+}
+return count
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int count = 0, left = 0, right = len - 1;
+while (left < right - 1) {//до схождения границ
+int middle = (left + right) / 2;//средний элемент
+if (arr[middle] <= value)//сужение массива
+left = middle;
+else
+right = middle;
+}
+len = right-1;
+for (int i = len; i >=0; i--) {
+for (int j = 0; j < i; j++) {
+if (arr[i] + arr[j] == value)
+count++;
+if (arr[i] + arr[j] > value)
+break;
+}
+}
+return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+int sym = 0;
+for (int i = 0; i < len - 1; i++) {
+int left = i, right = len;
+while (left < right - 1) {
+int mid = (left + right) / 2;
+if (arr[i] + arr[mid] == value) {
+sym++;
+int k = mid + 1;
+while (arr[i] + arr[k] == value && k < right) {
+sym++;
+k++;
+}
+k = mid ;
+while (arr[i] + arr[k] == value && k > left) {
+sym++;
+k--;
+}
+break;
+}
+if (arr[i] + arr[mid] > value)
+right = mid;
+else
+left = mid;
+}
+}
+return sym;
 }
